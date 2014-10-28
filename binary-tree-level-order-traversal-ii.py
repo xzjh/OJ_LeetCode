bd46cb945b4223f720341eb2234f08a0cf88c812
xzjh <jsxzjh@gmail.com>
2014-10-28 15:53:29 -0700

    binary-tree-level-order-traversal-ii

---
+++ binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,39 @@
+# Definition for a  binary tree node
+class TreeNode:
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+class Solution:
+	# @param root, a tree node
+	# @return a list of lists of integers
+	def levelOrderBottom(self, root):
+		if not root:
+			return []
+		q = [root]
+		qt = []
+		res = []
+		rest = []
+		while q:
+			node = q.pop(0)
+			rest.append(node.val)
+			if node.left:
+				qt.append(node.left)
+			if node.right:
+				qt.append(node.right)
+			if not q:
+				q = qt
+				qt = []
+				res.append(rest)
+				rest = []
+		res.reverse()
+		return res
+
+s = Solution()
+n1 = TreeNode(1)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n1.left = n2
+n1.right = n3
+print s.levelOrderBottom(n1)

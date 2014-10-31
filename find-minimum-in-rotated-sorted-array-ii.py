e4a5a78080ab95f88802f872afc84121b3a45881
xzjh <jsxzjh@gmail.com>
2014-10-31 00:23:22 -0700

    find-minimum-in-rotated-sorted-array-ii

---
+++ find-minimum-in-rotated-sorted-array-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+	# @param num, a list of integer
+	# @return an integer
+	def findMin(self, num):
+		if num == None or len(num) == 0:
+			return None
+		l = 0
+		h = len(num) - 1
+		while h >= l:
+			p = (h + l) / 2
+			if num[l] == num[h]:
+				l += 1
+				continue
+			if num[l] <= num[p] and num[p] <= num[h]:
+				return num[l]
+			elif num[l] > num[p]:
+				h = p
+				l += 1
+			elif num[p] > num[h]:
+				l = p + 1
+		return num[p]
+
+s = Solution()
+print s.findMin([3,3])

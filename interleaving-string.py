96cd3fd7f452c9085ed4f01849d9fd8f017c28b2
xzjh <jsxzjh@gmail.com>
2014-11-04 00:38:57 -0800

    interleaving-string

---
+++ interleaving-string.py
@@ -0,0 +1,24 @@
+class Solution:
+	# @return a boolean
+	def isInterleave(self, s1, s2, s3):
+		if len(s1) + len(s2) != len(s3):
+			return False
+		dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+		dp[0][0] = True
+		for i in range(len(s1)):
+			if s1[i] == s3[i]:
+				dp[i + 1][0] = dp[i][0]
+			else:
+				break
+		for j in range(len(s2)):
+			if s2[j] == s3[j]:
+				dp[0][j + 1] = dp[0][j]
+			else:
+				break
+		for i in range(len(s1)):
+			for j in range(len(s2)):
+					dp[i + 1][j + 1] = dp[i][j + 1] and s1[i] == s3[i + j + 1] or dp[i + 1][j] and s2[j] == s3[i + j + 1]
+		return dp[len(s1)][len(s2)]
+
+s = Solution()
+print s.isInterleave('','b','b')

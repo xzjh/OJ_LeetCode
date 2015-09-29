2e8e7cf3bf8d79d3a0006e05212ff8a35e9b8b0b
xzjh <jsxzjh@gmail.com>
2015-09-29 14:22:47 -0700

    ugly-number

---
+++ ugly-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+	def isUgly(self, num):
+		"""
+		:type num: int
+		:rtype: bool
+		"""
+		if num == 1:
+			return True
+		if num <= 0:
+			return False
+		
+		while num > 1:
+			if num % 2 == 0:
+				num //= 2
+				continue
+			if num % 3 == 0:
+				num //= 3
+				continue
+			if num % 5 == 0:
+				num //= 5
+				continue
+			return False
+		
+		return True
